22e2a61da132a8ab92b080e43f8fd7554fc60200
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2024-01-24 16:42:51 +0700

    Editing UI in chatting detail screen

--- components/UploadImageModal/UploadImageModal.tsx
+++ components/UploadImageModal/UploadImageModal.tsx
@@ -1,19 +1,27 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Button, FormControl, Input, Modal } from "native-base";
-
+import { Entypo } from "@expo/vector-icons";
 const UploadImageModal = (props: any) => {
   const { showModal, setShowModal, onPressCamera, onPressUploadImageGallery } =
     props;
   return (
     <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-      <Modal.Content maxWidth="400px">
+      <Modal.Content>
         <Modal.CloseButton />
         <Modal.Header>Chọn ảnh</Modal.Header>
         <Modal.Body>
-          <Button.Group space={2} justifyContent="center">
-            <Button onPress={onPressCamera}>Camera</Button>
-            <Button onPress={onPressUploadImageGallery}>Ảnh của bạn</Button>
+          <Button.Group space={10} justifyContent="center">
+            <Button onPress={onPressCamera} borderRadius={100} p={3}>
+              <Entypo name="camera" size={30} color="#fff" />
+            </Button>
+            <Button
+              onPress={onPressUploadImageGallery}
+              borderRadius={100}
+              p={3}
+            >
+              <Entypo name="images" size={30} color="#fff" />
+            </Button>
           </Button.Group>
         </Modal.Body>
       </Modal.Content>
