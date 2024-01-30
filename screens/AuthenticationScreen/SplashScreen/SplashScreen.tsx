77500ccbe19e0c47f4bfc1cc27a10c219f0ecfe7
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2024-01-30 20:18:30 +0700

    Change splash screen

--- screens/AuthenticationScreen/SplashScreen/SplashScreen.tsx
+++ screens/AuthenticationScreen/SplashScreen/SplashScreen.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
-import { Text, View, ActivityIndicator } from "react-native";
-
+import { ActivityIndicator } from "react-native";
+import { Text, View } from "native-base";
+import { appColor } from "../../../theme";
 export default function SplashScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Getting token...</Text>
+    <View
+      backgroundColor={appColor.background}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Text color="gray.600">Ứng dụng đang tải, vui lòng đợi...</Text>
       <ActivityIndicator size="large" />
     </View>
   );
